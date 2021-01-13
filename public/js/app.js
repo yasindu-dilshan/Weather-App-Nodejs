7e008cad660a52df049953d257a79c3a47a47dbb
yazi99 <56828636+yazi99@users.noreply.github.com>
2021-01-14 01:31:34 +0530

    setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -13,16 +13,14 @@ weatherForm.addEventListener("submit", (e) => {
 
   const location = search.value;
 
-  fetch("http://localhost:3000/weather?address=" + location).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          locationEl.textContent = data.error;
-        } else {
-          locationEl.textContent = data.location;
-          forecastEl.textContent = data.forecast;
-        }
-      });
-    }
-  );
+  fetch("/weather?address=" + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        locationEl.textContent = data.error;
+      } else {
+        locationEl.textContent = data.location;
+        forecastEl.textContent = data.forecast;
+      }
+    });
+  });
 });
--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, "../public");
@@ -70,11 +71,6 @@ app.get("/weather", (req, res) => {
       });
     }
   );
-  // res.send({
-  //   forecast: "It is snowing",
-  //   location: "Philadelphia",
-  //   address: req.query.address,
-  // });
 });
 
 app.get("/product", (req, res) => {
@@ -105,6 +101,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server is up on port 3000.");
+app.listen(port, () => {
+  console.log("Server is up on port " + port);
 });
